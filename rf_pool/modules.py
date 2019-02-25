39d9dc52c22053af627096ac115e18e00f577d35
Justin Theiss <jdeantheiss@gmail.com>
2019-02-25 11:48:45 -0800

    Updated ControlNetwork to provide greater flexibility in number of layers in trunk/branches. Also made updated to FeedForwardNetwork, Module to accommodate these changes.

--- rf_pool/modules.py
+++ rf_pool/modules.py
@@ -1,3 +1,4 @@
+#TODO: perhaps use nn.Sequential?
 import torch
 import torch.nn as nn
 import numpy as np
@@ -93,35 +94,42 @@ class Module(nn.Module):
         Parameters
         ----------
         net : Module
-            network to append to current network
+            network to append to current network (must not be trained)
         """
         assert type(net) == type(self), (
             'type of appended network must match current')
         assert self.n_layers is not None and net.n_layers is not None, (
             'networks must be initialized')
+        # update layer_ids
+        new_layer_ids = [str(i) for i in range(self.n_layers, self.n_layers+net.n_layers)]
+        net.update_layers(new_layer_ids)
+        
         # update n_layers
         self.n_layers += net.n_layers
         if not hasattr(net, 'attrs') or type(net.attrs) is not dict:
             raise Exception('appended network must contain "attrs" attribute')
+            
         # append each list var, update each dict/nn.ModuleDict var
         for (key, value) in net.attrs.items():
             if type(self.attrs[key]) is list:
                 self.attrs[key] = self.append_list_vars(self.attrs[key], value)
             elif hasattr(self.attrs[key], 'update'):
-                self.attrs[key].update({key: value})
+                self.attrs[key].update(value.items())
+
+        # update attributes
         self.update_attrs(self.attrs)
+        self.set_from_attrs()
+        
         # link new parameters
         for layer_id in net.layer_ids:
-            self.link_control_parameters(layer_id)
+            self.link_parameters(layer_id)
     
-    def link_control_parameters(self, layer_id):
-        # if no control nets, return
-        if not hasattr(self, 'control_nets') or not self.control_nets:
-            return
+    def link_parameters(self, layer_id, net_name='branch'):
         # set layer_id str, register each layer_id_net_name_param to self
+        if not hasattr(self, 'nets'):
+            return
         layer_id = str(layer_id)
-        net_names = self.set_list_vars(['trunk','branch'], len(self.control_nets[layer_id]))
-        for i, (net_name, net) in enumerate(zip(net_names, self.control_nets[layer_id])):
+        for i, net in enumerate(self.nets):
             for name, param in net.named_parameters():
                 reg_name = '_'.join([layer_id, net_name, str(i), name.replace('.','_')])
                 self.register_parameter(reg_name, param)
@@ -200,9 +208,6 @@ class Module(nn.Module):
         return dropout
     
     def make_layers(self):
-        pass
-
-    def link_parameters(self):
         pass
     
     def apply_forward_pass(self):
@@ -310,7 +315,7 @@ class FeedForwardNetwork(Module):
         attr_names = ['output_channels', 'kernel_sizes', 'pool_ksizes',
                       'control_nets', 'layer_names', 'act_names', 'pool_names', 
                       'dropout_names', 'hidden_layers', 'activations', 
-                      'pool_layers', 'dropouts']
+                      'pool_layers', 'dropouts', 'output_shapes']
         self.init_attrs(attr_names)
         # update self.attrs with kwargs, set_from_attrs
         self.update_attrs(kwargs)
@@ -332,7 +337,7 @@ class FeedForwardNetwork(Module):
         # activation functions
         self.act_types = self.set_list_vars(self.act_types, self.n_layers)
         self.act_names = self.get_typenames(self.act_types)
-
+        
         # pooling layer params
         self.pool_types = self.set_list_vars(self.pool_types, self.n_layers)
         self.pool_names = self.get_typenames(self.pool_types)
@@ -372,16 +377,50 @@ class FeedForwardNetwork(Module):
         for i, layer_id in enumerate(self.layer_ids):
             layer_id = str(layer_id)
             # layer types
-            self.hidden_layers[layer_id] = self.set_hidden_layer(i)
+            self.hidden_layers.add_module(layer_id, self.set_hidden_layer(i))
 
             # activation types
-            self.activations[layer_id] = self.set_activation_fn(i)
+            self.activations.add_module(layer_id, self.set_activation_fn(i))
 
             # pooling types
-            self.pool_layers[layer_id] = self.set_pool_layer(i)
+            self.pool_layers.add_module(layer_id, self.set_pool_layer(i))
 
             # dropout
-            self.dropouts[layer_id] = self.set_dropout(i)
+            self.dropouts.add_module(layer_id, self.set_dropout(i))
+
+    def update_layers(self, new_layer_ids):
+        # ensure new_layer_ids are string
+        new_layer_ids = [str(i) for i in new_layer_ids]
+        for layer_id, new_layer_id in zip(self.layer_ids, new_layer_ids):
+            # update hidden layers
+            if self.hidden_layers and layer_id in self.hidden_layers:
+                hidden_layer = self.hidden_layers.pop(layer_id)
+                self.hidden_layers.update({new_layer_id: hidden_layer})
+            
+            # update activations
+            if self.activations and layer_id in self.activations:
+                activation = self.activations.pop(layer_id)
+                self.activations.update({new_layer_id: activation})
+            
+            if self.control_nets and layer_id in self.control_nets:
+                control_net = self.control_nets.pop(layer_id)
+                self.control_nets.update({new_layer_id: control_net})
+            
+            # update pooling layers
+            if self.pool_layers and layer_id in self.pool_layers:
+                pool_layer = self.pool_layers.pop(layer_id)
+                self.pool_layers.update({new_layer_id: pool_layer})
+            
+            # update dropouts
+            if self.dropouts and layer_id in self.dropouts:
+                dropout = self.dropouts.pop(layer_id)
+                self.dropouts.update({new_layer_id: dropout})
+            
+        # set new layer_ids
+        self.layer_ids = new_layer_ids
+        
+        # update attributes
+        self.update_attrs()
 
     def apply_forward_pass(self, fn, x, *args):
         if fn:
@@ -395,8 +434,8 @@ class FeedForwardNetwork(Module):
         layer_id = str(layer_id)
         x = self.apply_forward_pass(self.hidden_layers[layer_id], x)
         x = self.apply_forward_pass(self.activations[layer_id], x)
-        if self.control_nets and layer_id in self.control_nets.layer_ids:
-            self.control_out = self.control_nets(layer_id, x)
+        if self.control_nets and layer_id in self.control_nets:
+            self.control_out = self.control_nets[layer_id](x)
         x = self.apply_forward_pass(self.pool_layers[layer_id], x, *self.control_out)
         x = self.apply_forward_pass(self.dropouts[layer_id], x)
         return x
@@ -413,100 +452,141 @@ class FeedForwardNetwork(Module):
     
 class ControlNetwork(Module):
     """
-    TODO: WRITEME
+    Module for building control network that can have a trunk network with multiple
+    branch networks. 
+    
+    Parameters
+    ----------
+    net : Module
+        network that control network will be connected to
+    layer_id : string
+        layer id within net that indicates the layer the control network connects to
+    output_shapes : list of tuples
+        expected output shape for each branch network (see example)
+    trunk : dict, optional
+        arguments to be passed to FeedForwardNetwork to create the trunk network
+        [default: [], no trunk network]
+    branches : list of dicts, optional
+        arguments to be passed to FeedForwardNetwork to create each branch network.
+        additionally, a fully connected layer will be appended to each branch network
+        to obtain the number of units indicated by np.prod(output_shapes[b][1:])
+        [default: [], single fully connected layer]
+    branch_act_types : list, optional kwarg
+        activation types for each branch to be applied to final output (see FeedForwardNetwork)
+        [default: [None]*n_branches]
+        
+    Attributes
+    ----------
+    nets : list of Module
+        trunk and branch networks that are called during forward() method
+    layer_id : str
+        layer id to which control network connects
+    n_branches : int
+        number of branches in control network (n_branches = len(output_shapes))
+    
+    Methods
+    -------
+    get_conv_shape(output_shape, pool_ksize)
+        return output shape of convolutional layer prior to pooling
+    make_layers(net, layer_id, output_shapes, trunk, branches, branch_act_types)
+        create trunk and branch networks (attribute nets)
+    forward(x)
+        apply forward pass through control network, which passes the output of the 
+        trunk network to each branch network followed by reshaping each branch output
+        to shape of each output_shapes
+
+    Examples
+    --------
+    # creates control network to output mu and sigma deltas for a receptive field pool layer
+    >>> rf_layer = RF_Pool(torch.rand(36,2), torch.ones(36,1), img_shape=(24,24), pool_type='sum')
+    >>> net = FeedForwardNetwork((1,1,28,28), ['conv'], ['relu'], [rf_layer], output_channels=[20],
+                                kernel_sizes=[5], pool_ksizes=[2])
+    >>> trunk = {'layer_types': ['fc'], 'act_types': ['relu'], 'output_channels': [128]}
+    >>> branches = []
+    >>> net.control_nets = nn.ModuleDict()
+    >>> net.control_nets['0'] = ControlNetwork(net, '0', [(-1,1,36,2), (-1,1,36,1)], 
+                                               trunk=trunk, branches=branches)
     """
-    def __init__(self, net, layer_ids, layer_types, branch_output_shapes, 
-                 act_types=['relu'], branch_act_types=[None,None], **kwargs):
+    def __init__(self, net, layer_id, output_shapes, trunk=[], branches=[], **kwargs):
         super(ControlNetwork, self).__init__()
         # set inputs to attributes
-        self.update_attrs({'layer_ids':layer_ids, 'layer_types':layer_types,
-                           'branch_output_shapes':branch_output_shapes,
-                           'act_types':act_types, 'branch_act_types':branch_act_types})
+        self.update_attrs({'layer_id':layer_id, 'output_shapes':output_shapes})
         # initialize additional attributes
-        attr_names = [key for key in net.attrs.keys()]
-        self.init_attrs(attr_names)
+        attr_names = ['branch_act_types']
+        self.init_attrs(attr_names, [None])
+        
         # update self.attrs with kwargs, set_from_attrs
         self.update_attrs(kwargs)
         self.set_from_attrs()
         
-        assert type(self.layer_types) is list, (
-            "layer_types must be list")
-        assert self.layer_types[-1] == 'fc', (
-            "The last trunk layer must be fully-connected")
-        for layer_id in self.layer_ids:
-            layer_id = int(layer_id)
-            assert len(net.output_shapes[layer_id]) == 4, (
-                "Control networks must follow conv layers")
-
-        # trunk params
-        self.layer_ids = [str(i) for i in self.layer_ids]
-        self.n_layers = len(self.layer_types)
-        self.output_channels = self.set_list_vars(self.output_channels, self.n_layers)
-        self.kernel_sizes = self.set_list_vars(self.kernel_sizes, self.n_layers)
-        self.act_types = self.set_list_vars(self.act_types, self.n_layers)
+        # set layer_ids, check net_params
+        self.layer_id = str(self.layer_id)
+        if type(self.output_shapes) is not list:
+            self.output_shapes = [self.output_shapes]
+        self.n_branches = len(self.output_shapes)
+        branches = self.set_list_vars(branches, self.n_branches)
+        self.branch_act_types = self.set_list_vars(self.branch_act_types, self.n_branches)
         
-        # branch params
-        if type(self.branch_output_shapes) is not list:
-            self.branch_output_shapes = [self.branch_output_shapes]
-        self.branch_act_types = self.set_list_vars(self.branch_act_types, 
-                                                   len(self.branch_output_shapes))
-        
-        # initialize control nets
-        self.make_layers(net)
+        # make layers
+        self.make_layers(net, self.layer_id, self.output_shapes, trunk, branches,
+                         self.branch_act_types)
         
         # link parameters
-        for layer_id in self.layer_ids:
-            self.link_control_parameters(layer_id)
-
+        self.link_parameters(self.layer_id)
+        
         # update attrs
         self.update_attrs()
     
-    def get_conv_shape(self, output_shape, pool_ksize):
+    def get_conv_shape(self, output_shape, pool_ksize): 
+        #TODO: if pool_layer is set directly, pool_ksize may not be accurate
         if pool_ksize is None:
             return output_shape
         return output_shape[:2] + (output_shape[-2]*pool_ksize, output_shape[-1]*pool_ksize)
         
-    def make_layers(self, net):
-        # build control networks
-        self.control_nets = {}
-        for i, layer_id in enumerate(self.layer_ids):
-            # get layer shape prior to pooling
-            layer_shape = self.get_conv_shape(net.output_shapes[int(layer_id)], 
-                                              net.pool_ksizes[int(layer_id)])
-            # make the trunk
-            trunk = self.make_trunk(layer_shape)
-            
-            # make branches
-            branches = []
-            for b, branch_output_shape in enumerate(self.branch_output_shapes):
-                branches.append(self.make_branch(trunk.output_shapes[-1], self.branch_act_types[b],
-                                                 branch_output_shape))
-                
-            # set control net
-            self.control_nets[str(layer_id)] = (trunk, *branches)
+    def make_layers(self, net, layer_id, output_shapes, trunk, branches, branch_act_types):
+        # get layer shape prior to pooling
+        input_shape = self.get_conv_shape(net.output_shapes[int(layer_id)], 
+                                          net.pool_ksizes[int(layer_id)])
         
-    def make_trunk(self, input_shape):
-        # build trunk for control network
-        trunk_net = FeedForwardNetwork(input_shape, self.layer_types, self.act_types,
-                                       output_channels=self.output_channels, 
-                                       kernel_sizes=self.kernel_sizes)
-        return trunk_net
-
-    def make_branch(self, input_shape, act_type, output_channels):
-        # build branch for control network with shape (batch, output_channels) 
-        output_channels = np.prod(output_channels[1:])
-        branch_net = FeedForwardNetwork(input_shape, ['fc'], [act_type], 
-                                        output_channels=[output_channels])
-        return branch_net
-
-    def forward(self, layer_id, x):
-        layer_id = str(layer_id)
-        trunk = self.control_nets[layer_id][0]
-        branches = self.control_nets[layer_id][1:]
-        trunk_out = trunk.forward(x)
+        # init nets
+        self.nets = []
+        
+        # make trunk
+        if trunk:
+            trunk_net = FeedForwardNetwork(input_shape, **trunk)
+            self.nets.append(trunk_net)
+            branch_input_shape = trunk_net.output_shapes[-1]
+        else:
+            branch_input_shape = input_shape
+            
+        # make branches
+        for b in range(len(output_shapes)):
+            # append fc with output_channels = np.prod(output_shapes[b][1:])
+            output_channels = np.prod(output_shapes[b][1:])
+            
+            # set trunk, append branch
+            if branches:
+                self.nets.append(FeedForwardNetwork(branch_input_shape, **branches[b]))
+                self.nets[-1].append(FeedForwardNetwork(self.nets[-1].output_shapes[-1], 
+                                                        ['fc'], [branch_act_types[b]], 
+                                                        output_channels=output_channels))
+            else: # set branch with single layer
+                self.nets.append(FeedForwardNetwork(branch_input_shape, ['fc'], [branch_act_types[b]], 
+                                                    output_channels=output_channels))
+            
+    def forward(self, x):
+        # get nets
+        if self.n_branches < len(self.nets):
+            trunk_net = self.nets[0]
+            branch_nets = self.nets[1:]
+            # get trunk output
+            x = trunk_net.forward(x)
+        else:
+            branch_nets = self.nets
+        # set branch outputs
         branch_out = []
-        for branch, branch_output_shape in zip(branches, self.branch_output_shapes):
-            branch_out.append(torch.reshape(branch.forward(trunk_out), branch_output_shape))
+        for branch_net, output_shape in zip(branch_nets, self.output_shapes):
+            branch_out.append(torch.reshape(branch_net.forward(x), output_shape))
         return branch_out
 
 class GenerativeNetwork(Module):
