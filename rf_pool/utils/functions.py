ce6cb480f9d6d68928517713b2ea189ab0351629
Justin Theiss <jdeantheiss@gmail.com>
2019-03-24 21:41:48 -0700

    Created functions.py utils for various functions (e.g., custom repeat)

---
+++ rf_pool/utils/functions.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+
+def repeat(x, repeats):
+    """
+    Perform numpy-like repeat function
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        tensor to be repeated across dimensions
+    repeats : tuple or list
+        number of repeats for each dimension
+
+    Returns
+    -------
+    y : torch.Tensor
+        repeated tensor
+
+    Examples
+    --------
+    >>> x = torch.as_tensor([[1.,2.],[3.,4.]])
+    >>> y = repeat(x, (2, 3))
+    >>> print(y)
+    tensor([[1., 1., 1., 2., 2., 2.],
+            [1., 1., 1., 2., 2., 2.],
+            [3., 3., 3., 4., 4., 4.],
+            [3., 3., 3., 4., 4., 4.]])
+    """
+    y = x.numpy()
+    for i, r in enumerate(repeats):
+        y = np.repeat(y, r, i)
+    return torch.as_tensor(y)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
