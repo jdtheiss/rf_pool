bcb70674fe6e072c1cf527bf03e6553ef963e923
Justin Theiss <jdeantheiss@gmail.com>
2019-05-11 11:00:45 -0700

    Added vector_norm function and added 1e-6 to norm during cosine similarity division to avoid NaNs

--- rf_pool/utils/visualize.py
+++ rf_pool/utils/visualize.py
@@ -1,115 +1,122 @@
 import torch
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib import offsetbox
+
+def vector_norm(x):
+    return torch.sqrt(torch.sum(torch.pow(x, 2), 1))
 
 def rf_interference(feat_i, feat_j):
     """
     Computes a receptive field interference score:
-    mena cosine similarity between feat_i and rf_pool(feat_i, feat_j)
-    
+    mean cosine similarity between feat_i and max(feat_i, feat_j)
+
     Parameters
     ----------
     feat_i: torch.tensor
         features corresponding to first class label
     feat_j: torch.tensor
         features corresponding to second class label
-        
+
     Returns
     -------
     mean: torch.tensor
-        the average cosine similarity 
+        the average cosine similarity
     std: torch.tensor
-        std dev of cosine similarities 
+        std dev of cosine similarities
     """
     n_i = feat_i.shape[0]
     n_j = feat_j.shape[0]
     feat_i_r = torch.unsqueeze(feat_i, -1).repeat(1,1,n_j)
     feat_j_r = torch.unsqueeze(feat_j, -1).repeat(1,1,n_i)
-    
+
     # max across channels
     feat_p = torch.max(feat_i_r, feat_j_r.permute(2,1,0))
-    
+
     # inner product
-    inner = torch.sum(feat_i_r * feat_p, dim=1)
-    
+    inner = torch.sum(torch.mul(feat_i_r, feat_p), 1)
+
     # norms
-    norm_i = torch.sqrt(torch.diagonal(torch.matmul(feat_i, torch.t(feat_i))))
+    norm_i = vector_norm(feat_i)
     norm_i = torch.unsqueeze(norm_i, -1).repeat(1,n_j)
-    norm_j = torch.sqrt(torch.sum(torch.pow(feat_p, 2), dim=1))
-    norm = norm_i * norm_j
-    
+    norm_j = vector_norm(feat_p)
+    norm = torch.mul(norm_i, norm_j)
+
     # average over cosine similarity
-    cosine_sim = inner / norm
-    
+    cosine_sim = torch.div(inner, norm + 1e-6)
+
     return 1 - torch.mean(cosine_sim), torch.std(cosine_sim)
 
 def pairwise_cosine_similarity(feat_i, feat_j):
     """
     Computes pair-wise cosine similarity
     between features in feat_i and feat_j
-    
+
     Parameters
     ----------
     feat_i: torch.tensor
         features corresponding to class label i
     feat_j: torch.tensor
         features corresponding to class label j
-        
+
     Returns
     -------
     mean: torch.tensor
-        the average cosine similarity 
+        the average cosine similarity
     std: torch.tensor
-        std dev of cosine similarities 
+        std dev of cosine similarities
     """
+    # inner product
+    inner = torch.sum(torch.mul(feat_i, feat_j), 1)
 
-    inner = torch.sum(torch.mul(feat_i, feat_j), dim=1)
-    norm_i = torch.sqrt(torch.sum(torch.pow(feat_i, 2), dim=1))
-    norm_j = torch.sqrt(torch.sum(torch.pow(feat_j, 2), dim=1))
-    
-    cosine_sim = torch.div(inner, torch.mul(norm_i, norm_j))
-    
+    # norms
+    norm_i = vector_norm(feat_i)
+    norm_j = vector_norm(feat_j)
+    norm = torch.mul(norm_i, norm_j)
+
+    # avarage over the cosine similarity
+    cosine_sim = torch.div(inner, norm + 1e-6)
+
     return torch.mean(cosine_sim), torch.std(cosine_sim)
-    
+
 def cosine_similarity(feat_i, feat_j):
     """
     Computes the mean cosine similarity across
-    every permutation pair of the features 
+    every permutation pair of the features
     in feat_i and feat_j
-    
+
     Parameters
     ----------
     feat_i: torch.tensor
         features corresponding to class label i
     feat_j: torch.tensor
         features corresponding to class label j
-        
+
     Returns
     -------
     mean: torch.tensor
-        the average cosine similarity 
+        the average cosine similarity
     std: torch.tensor
-        std dev of cosine similarities 
+        std dev of cosine similarities
     """
     # compute inner products
-    inner = torch.matmul(feat_i, torch.t(feat_j))
-    
+    inner = torch.matmul(feat_i, feat_j.t())
+
     # calculate the norms
-    norm_i = torch.sqrt(torch.diagonal(torch.matmul(feat_i, torch.t(feat_i)))).reshape(1,-1)
-    norm_j = torch.sqrt(torch.diagonal(torch.matmul(feat_j, torch.t(feat_j)))).reshape(1,-1)
-    norm = torch.matmul(torch.t(norm_i), norm_j)
-    
-    # avarage over the cosine similarity 
-    cosine_sim = inner/norm 
-    
+    norm_i = vector_norm(feat_i).reshape(1,-1)
+    norm_j = vector_norm(feat_j).reshape(1,-1)
+    norm = torch.matmul(norm_i.t(), norm_j)
+
+    # avarage over the cosine similarity
+    cosine_sim = torch.div(inner, norm + 1e-6)
+
     return torch.mean(cosine_sim), torch.std(cosine_sim)
 
 def confusion_matrix(feature_vectors, labels, interference_fn=cosine_similarity):
     """
     Computes a mean cosine similarity matrix
     from a set of feature vectors and corresponding class labels
-    
+
     Parameters
     ----------
     feature_vectors: torch.tensor
@@ -128,16 +135,16 @@ def confusion_matrix(feature_vectors, labels, interference_fn=cosine_similarity)
     unique_labels = torch.unique(labels, sorted=True)
     mean_matrix = torch.zeros((len(unique_labels),)*2)
     std_matrix = torch.zeros((len(unique_labels),)*2)
-    
+
     for i, label_i in enumerate(unique_labels):
         for j, label_j in enumerate(unique_labels):
             feat_i = feature_vectors[np.where(labels == label_i)[0]]
-            feat_j = feature_vectors[np.where(labels == label_j)[0]] 
+            feat_j = feature_vectors[np.where(labels == label_j)[0]]
             # compute the mean cosine similarity cosine
             mean_matrix[i, j], std_matrix[i,j]  = interference_fn(feat_i, feat_j)
-    
+
     return mean_matrix, std_matrix, unique_labels
-    
+
 def show_confusion_matrix(data, labels, cmap=plt.cm.afmhot):
     """
     TODO
@@ -146,19 +153,19 @@ def show_confusion_matrix(data, labels, cmap=plt.cm.afmhot):
     heatmap = ax.pcolor(data, cmap=cmap)
     ax.set_xticks(np.arange(data.shape[0])+0.5, minor=False)
     ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
-    
+
     ax.invert_yaxis()
     ax.xaxis.tick_top()
 
     ax.set_xticklabels(labels.numpy(), minor=False)
-    ax.set_yticklabels(labels.numpy(), minor=False) 
+    ax.set_yticklabels(labels.numpy(), minor=False)
     fig.colorbar(heatmap)
 
 def visualize_embedding(embeddings, images, labels=None, cmap='tab10', figsize=(15,15)):
     """
-    2D Visualization for a dimensionally-reduced set of embeddings. 
-    Plots the images at the embedding points 
-    
+    2D Visualization for a dimensionally-reduced set of embeddings.
+    Plots the images at the embedding points
+
     Paramaters
     ----------
     embeddings: numpy.array
@@ -190,7 +197,7 @@ def visualize_embedding(embeddings, images, labels=None, cmap='tab10', figsize=(
         if np.min(dist_to_embeddings) >= min_dist:
             shown_embeddings = np.vstack((shown_embeddings, embeddings[i,None]))
             patch = images[i]
-            
+
             # colorize images
             if labels is not None:
                 if images.shape[-1] != 3:
@@ -213,9 +220,5 @@ def visualize_embedding(embeddings, images, labels=None, cmap='tab10', figsize=(
 
 def color_code_patch(patch, label, cmap):
     patch = np.tile(patch, (1,1,3))
-    patch = (1,1,1) * (1 - patch) + cmap(label)[:3] * patch 
+    patch = (1,1,1) * (1 - patch) + cmap(label)[:3] * patch
     return np.clip(patch, 0, 1)
-
-
-
-
