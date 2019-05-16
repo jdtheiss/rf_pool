fe1c74aa7f6103eed2e1b7d30a6c239c2d81a39b
Justin Theiss <jdeantheiss@gmail.com>
2019-05-16 12:14:06 -0500

    Updated mask_kernel_2d and mask_kernel_lattice functions to allow for optional threshold argument

--- rf_pool/utils/lattice.py
+++ rf_pool/utils/lattice.py
@@ -82,9 +82,9 @@ def dog_kernel_2d(mu, sigma, ratio, xy):
     kernel_1 = gaussian_kernel_2d(mu, ratio * sigma, xy)
     return kernel_0 - kernel_1
 
-def mask_kernel_2d(mu, sigma, xy):
+def mask_kernel_2d(mu, sigma, xy, thr=np.exp(-0.5)):
     kernels = exp_kernel_2d(mu, sigma, xy)
-    thr = torch.exp(torch.as_tensor(-0.5, dtype=kernels.dtype))
+    thr = torch.tensor(thr, dtype=kernels.dtype)
     mask = torch.as_tensor(torch.ge(kernels, thr), dtype=kernels.dtype)
     with torch.no_grad():
         kernels_no_grad = torch.add(kernels, 1e-6)
@@ -313,7 +313,7 @@ def dog_kernel_lattice(mu, sigma, kernel_shape, ratio=4.):
 
     return dog_kernel_2d(mu, sigma, ratio, xy)
 
-def mask_kernel_lattice(mu, sigma, kernel_shape):
+def mask_kernel_lattice(mu, sigma, kernel_shape, thr=np.exp(-0.5)):
     """
     Returns a tensor of masked kernels
 
@@ -325,6 +325,9 @@ def mask_kernel_lattice(mu, sigma, kernel_shape):
         kernel standard deviations with shape (n_kernels, 1)
     kernel_shape : tuple
         shape of input feature map
+    thr : float, optional
+        threshold for masking pixels based on exp_kernel_2d values
+        [default: np.exp(-0.5) i.e. 1 sigma]
 
     Returns
     -------
@@ -347,7 +350,7 @@ def mask_kernel_lattice(mu, sigma, kernel_shape):
     y = torch.arange(kernel_shape[1])
     xy = torch.stack(torch.meshgrid(x, y), dim=0).unsqueeze(0).float()
 
-    return mask_kernel_2d(mu, sigma, xy)
+    return mask_kernel_2d(mu, sigma, xy, thr=thr)
 
 def init_foveated_lattice(img_shape, scale, spacing, n_rf=None, n_rings=None,
                           min_ecc=1., offset=[0.,0.], rotate_rings=False):
