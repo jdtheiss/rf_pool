d4e09403333893d1ae63c974ffd5b1d4e040b2a0
Justin Theiss <jdeantheiss@gmail.com>
2019-10-18 10:43:06 -0700

    Updated URLDataset to allow for url_replace to replace parts of urls found in webpage

--- rf_pool/utils/datasets.py
+++ rf_pool/utils/datasets.py
@@ -25,6 +25,7 @@ class Dataset(torch.utils.data.Dataset):
         self.timeout = 5.
         self.find_img_url = False
         self.url_pattern = '"([^\s]+\.jpg)"'
+        self.url_replace = ['','']
         self.load_fn = imageio.imread
         self.data_info = {}
         self.data = None
@@ -70,13 +71,14 @@ class Dataset(torch.utils.data.Dataset):
             elif key == pattern:
                 self.data_info.update({key: label})
 
-    def get_img_url(self, url, pattern='', timeout=5.):
+    def get_img_url(self, url, pattern='', replace=['',''], timeout=5.):
         try:
             with urllib.request.urlopen(url, timeout=timeout) as response:
                 html = response.read()
                 img_url = re.findall(pattern, html.decode('utf-8'))
             if len(img_url) > 0:
                 img_url = img_url[0]
+                img_url = re.sub(*replace, img_url)
             else:
                 img_url = None
         except Exception as detail:
@@ -91,9 +93,6 @@ class Dataset(torch.utils.data.Dataset):
             if os.path.isfile(fname):
                 return self.load_fn(fname)
             # otherwise load load from html
-            url = url.replace('\\', '')
-            if not url.startswith('http'):
-                url = 'http:' + url
             req = urllib.request.Request(url, headers={'User-Agent': 'Magic Browser'})
             with urllib.request.urlopen(req, timeout=timeout) as response:
                 html = response.read()
@@ -165,6 +164,7 @@ class Dataset(torch.utils.data.Dataset):
         # get url within html if necessary
         if self.find_img_url and type(img) is str:
             img = self.get_img_url(img, pattern=self.url_pattern,
+                                   replace=self.url_replace,
                                    timeout=self.timeout)
         # download if necessary
         if not self.download and type(img) is str:
