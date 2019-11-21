5c4e9831d553badd7221534909452f2f5b1ed15d
Justin Theiss <jdeantheiss@gmail.com>
2019-11-20 18:33:16 -0800

    Updated experiment_functions and Crowding_Experiment_Digits_Results to reflect updated heatmap computation

--- experiments/crowding_experiment/experiment_functions.py
+++ experiments/crowding_experiment/experiment_functions.py
@@ -72,8 +72,8 @@ def apply_attention_field(model, layer_id, mu, sigma, loc, extent):
     model.layers[layer_id].forward_layer.pool.update_rfs(mu=new_mu, sigma=new_sigma)
     return model
 
-def get_SNR_accuracy(target_loader, crowd_loader, peak, layer_id='1',batch_size=1,model=None, RF_mask=None, 
-                     extent=None, lattice_fn=None, lattice_kwargs=None):
+def get_accuracy(target_loader, crowd_loader, layer_id='1',batch_size=1, model=None, RF_mask=None,
+                 extent=None, lattice_fn=None, lattice_kwargs=None):
     """
     Gets the PSNR and accuracy scores for each receptive field for the whole set of stimuli 
     """
@@ -87,15 +87,10 @@ def get_SNR_accuracy(target_loader, crowd_loader, peak, layer_id='1',batch_size=
             rotate_fn = rotate
     else:
         rotate_fn = None
-    # init SNR, acc, counters
-    SNR = torch.zeros(1)
+    # init acc, counters
     correct = torch.zeros(1)
-    SNR_is = []
     acc_is = []
     cnt = 0.
-    # set mask_i if given
-    if RF_mask is not None:
-        mask_i = RF_mask.clone()
     # get SNR, accuracy for each image
     for i, ((target, labels), (crowd, _)) in enumerate(zip(target_loader, crowd_loader)):
         # reset RFs
@@ -106,37 +101,28 @@ def get_SNR_accuracy(target_loader, crowd_loader, peak, layer_id='1',batch_size=
                 mu, sigma = lattice_fn(**lattice_kwargs) 
             model.layers[layer_id].forward_layer.pool.update_rfs(mu=mu, sigma=sigma)
         # get mask
-        if RF_mask is None:
+        if RF_mask is not None:
+            mask_i = RF_mask.clone()
+        else:
             mask_i = model.rf_index(target, layer_id, thr=0.1).float()
         # attention
         if extent:
             model = apply_attention_field(model, layer_id, mu, sigma, [26,26], extent)
         # get target signal and noise (crowd)
         with torch.no_grad():
-            target_output = model.rf_output(target, layer_id, retain_shape=True)
-            signal = torch.max(target_output.flatten(-2), -1)[0] + torch.rand(target_output.shape[:-2])
             crowd_output = model.rf_output(crowd, layer_id, retain_shape=True)
-            noise = torch.max(crowd_output.flatten(-2), -1)[0] + torch.rand(crowd_output.shape[:-2])
             # mask crowd_output and pass forward to get accuracy
             masked_output = torch.max(torch.mul(crowd_output, mask_i.reshape(batch_size, 1, -1, 1, 1)), 2)[0]
             output = torch.max(model.apply_layers(masked_output, ['2']).flatten(-2), -1)[0]
             correct_i = torch.sum(torch.max(output, -1)[1] == labels).item()
             correct += correct_i
-        # peak snr numerator and denominator
-        MSE = torch.mean(torch.pow(signal - noise, 2), dim=[1])
-        # compute SNR
-        SNR_i = 10. * torch.log10(torch.div(torch.pow(peak, 2), MSE))
-        SNR_i = torch.mul(SNR_i, mask_i)
-        SNR = SNR + torch.sum(SNR_i)
-        SNR_is.append(torch.div(torch.sum(SNR_i), torch.sum(mask_i)).item())
         acc_is.append(correct_i)
         cnt += torch.sum(mask_i).item()
     # get pct correct, SNR
     pct_correct = (correct / len(target_loader)).item()
-    avg_SNR = torch.div(SNR, cnt).item()
-    return (avg_SNR, pct_correct, SNR_is, acc_is)
+    return (pct_correct, acc_is)
 
-def get_heatmaps(target_loader, crowd_loader, peak, layer_id='1', batch_size=1, model=None, RF_mask=None, 
+def get_heatmaps(target_loader, crowd_loader, layer_id='1', batch_size=1, model=None, RF_mask=None, acc=None,
                  extent=None, lattice_fn=None, lattice_kwargs=None):
     """
     Gets the PSNR values for each receptive field for the heat maps 
@@ -151,41 +137,45 @@ def get_heatmaps(target_loader, crowd_loader, peak, layer_id='1', batch_size=1, 
             rotate_fn = rotate
     else:
         rotate_fn = None
-    # init SNR, mask
+    # counter
+    cnt = 0.
     n_kernels = model.layers[layer_id].forward_layer.pool.mu.shape[0]
-    SNR = torch.zeros(1, n_kernels)
-    mask = torch.zeros(1, n_kernels)
-    # set mask_i if given
-    if RF_mask is not None:
-        mask_i = RF_mask.clone()
-    # get SNR heatmap for each image
-    for i, ((target, labels), (crowd, _)) in enumerate(zip(target_loader, crowd_loader)):
-        # update RFs
+    RF_acc = torch.zeros(n_kernels)
+    # get SNR, accuracy for each image
+    for i, ((target, label), (crowd, _)) in enumerate(zip(target_loader, crowd_loader)):
+        # skip wrong trials
+        if acc is not None and acc[i] == 0:
+            continue
+        # reset RFs
         if lattice_fn is not None and lattice_kwargs is not None:
             if rotate_fn is not None:
                 mu, sigma = lattice_fn(**lattice_kwargs, rotate=rotate_fn())
             else:
-                mu, sigma = lattice_fn(**lattice_kwargs)
+                mu, sigma = lattice_fn(**lattice_kwargs) 
             model.layers[layer_id].forward_layer.pool.update_rfs(mu=mu, sigma=sigma)
-        # get mask_i, add to mask
-        if RF_mask is None:
+        # get mask
+        if RF_mask is not None:
+            mask_i = RF_mask.clone()
+        else:
             mask_i = model.rf_index(target, layer_id, thr=0.1).float()
-        mask = mask + mask_i
         # attention
         if extent:
             model = apply_attention_field(model, layer_id, mu, sigma, [26,26], extent)
         # get target signal and noise (crowd)
         with torch.no_grad():
-            target_output = model.rf_output(target, layer_id, retain_shape=True)
-            signal = torch.max(target_output.flatten(-2), -1)[0] + torch.rand(target_output.shape[:-2])
             crowd_output = model.rf_output(crowd, layer_id, retain_shape=True)
-            noise = torch.max(crowd_output.flatten(-2), -1)[0] + torch.rand(crowd_output.shape[:-2])
-        # get MSE between target and crowding stimuli
-        MSE = torch.mean(torch.pow(signal - noise, 2), dim=[1])
-        # compute SNR
-        SNR_i = 10. * torch.log10(torch.div(torch.pow(peak, 2), MSE))
-        SNR_i = torch.mul(SNR_i, mask_i)
-        SNR = SNR + SNR_i
-    # get heatmap
-    SNR_hm = torch.div(SNR, mask)
-    return SNR_hm
+            # mask crowd_output and pass forward to get accuracy
+            masked_output = torch.mul(crowd_output, mask_i.reshape(batch_size, 1, -1, 1, 1))
+            # permute dimensions to (n_RF, n_ch, h, w)
+            masked_output = masked_output[0].permute(1,0,2,3)
+            # get flattened output of third layer (n_RF, n_ch, h * w)
+            output = torch.flatten(model.apply_layers(masked_output, ['2']), -2)
+            # get max index in image space for target channel (after summing out RFs)
+            max_idx = torch.max(torch.sum(output, 0)[label], -1)[1]
+            # get relative contribution to max value across RFs
+            max_output = output[:, label.item(), max_idx.item()]
+            RF_acc += torch.div(max_output, torch.sum(max_output))
+            cnt += 1.
+    # update RF_acc as proportion correct
+    RF_acc /= cnt
+    return RF_acc
