b16a6a2a1e8d8c60337d03d589a012364d154769
Justin Theiss <jdeantheiss@gmail.com>
2019-11-21 22:55:54 -0800

    Updated with get_contribution and get_ablated functions

--- experiments/crowding_experiment/experiment_functions.py
+++ experiments/crowding_experiment/experiment_functions.py
@@ -72,7 +72,7 @@ def apply_attention_field(model, layer_id, mu, sigma, loc, extent):
     model.layers[layer_id].forward_layer.pool.update_rfs(mu=new_mu, sigma=new_sigma)
     return model
 
-def get_accuracy(target_loader, crowd_loader, layer_id='1',batch_size=1, model=None, RF_mask=None,
+def get_accuracy(target_loader, crowd_loader, layer_id='1', batch_size=1, model=None, RF_mask=None,
                  extent=None, lattice_fn=None, lattice_kwargs=None):
     """
     Gets the PSNR and accuracy scores for each receptive field for the whole set of stimuli 
@@ -91,7 +91,7 @@ def get_accuracy(target_loader, crowd_loader, layer_id='1',batch_size=1, model=N
     correct = torch.zeros(1)
     acc_is = []
     cnt = 0.
-    # get SNR, accuracy for each image
+    # get accuracy for each image
     for i, ((target, labels), (crowd, _)) in enumerate(zip(target_loader, crowd_loader)):
         # reset RFs
         if lattice_fn is not None and lattice_kwargs is not None:
@@ -122,11 +122,12 @@ def get_accuracy(target_loader, crowd_loader, layer_id='1',batch_size=1, model=N
     pct_correct = (correct / len(target_loader)).item()
     return (pct_correct, acc_is)
 
-def get_heatmaps(target_loader, crowd_loader, layer_id='1', batch_size=1, model=None, RF_mask=None, acc=None,
-                 extent=None, lattice_fn=None, lattice_kwargs=None):
+def get_contribution(target_loader, crowd_loader, layer_id='1', model=None, RF_mask=None, acc=None,
+                     extent=None, lattice_fn=None, lattice_kwargs=None):
     """
-    Gets the PSNR values for each receptive field for the heat maps 
+    Gets the relative contribution for each receptive field to target channel output
     """
+    assert iter(target_loader).next()[0].shape[0] == 1, ('Batch size must be 1.')
     # parse lattice_kwargs
     if lattice_kwargs is not None:
         lattice_kwargs.setdefault('rotate', 0.)
@@ -161,23 +162,97 @@ def get_heatmaps(target_loader, crowd_loader, layer_id='1', batch_size=1, model=
         # attention
         if extent:
             model = apply_attention_field(model, layer_id, mu, sigma, [26,26], extent)
-        # get target signal and noise (crowd)
+        # get output for each RF
         with torch.no_grad():
-            crowd_output = model.rf_output(crowd, layer_id, retain_shape=True)
+            crowd_output = model.rf_output(target + crowd, layer_id, retain_shape=True)
             # mask crowd_output and pass forward to get accuracy
-            masked_output = torch.mul(crowd_output, mask_i.reshape(batch_size, 1, -1, 1, 1))
+            masked_output = torch.mul(crowd_output, mask_i.reshape(1, 1, -1, 1, 1))
             # permute dimensions to (n_RF, n_ch, h, w)
             masked_output = masked_output[0].permute(1,0,2,3)
             # get flattened output of third layer (n_RF, n_ch, h * w)
             output = torch.flatten(model.apply_layers(masked_output, ['2']), -2)
             output = torch.mul(output, mask_i.reshape(-1, 1, 1))
+            # get control output to account for target-flanker feature coincidences
+            crowd_control_output = model.rf_output(crowd, layer_id, retain_shape=True)
+            # mask crowd_output and pass forward to get accuracy
+            masked_control_output = torch.mul(crowd_control_output, mask_i.reshape(1, 1, -1, 1, 1))
+            # permute dimensions to (n_RF, n_ch, h, w)
+            masked_control_output = masked_control_output[0].permute(1,0,2,3)
+            # get flattened output of third layer (n_RF, n_ch, h * w)
+            control_output = torch.flatten(model.apply_layers(masked_control_output, ['2']), -2)
+            control_output = torch.mul(control_output, mask_i.reshape(-1, 1, 1))
+            # remove control_output from output (to account for accidental target features from flankers)
+            output = output - control_output
+            output[torch.lt(output, 0.)] = 0.
             # get max index in image space for target channel (after summing out RFs)
-            max_idx = torch.max(torch.sum(output, 0)[label], -1)[1]
+            max_idx = torch.sum(torch.sum(output, 0)[label], -1)
             # get relative contribution to max value across RFs
-            max_output = output[:, label.item(), max_idx.item()]
-            max_output[torch.lt(max_output, 0.)] = 0.
+            max_output = torch.sum(output[:, label.item()], -1)
             RF_acc += torch.div(max_output, torch.sum(max_output))
             cnt += 1.
     # update RF_acc as proportion correct
     RF_acc /= cnt
     return RF_acc
+
+def get_ablated(target_loader, crowd_loader, layer_id='1', model=None, RF_mask=None,
+                extent=None, lattice_fn=None, lattice_kwargs=None):
+    """
+    Gets the change in accuracy due to each receptive field
+    """
+    assert iter(target_loader).next()[0].shape[0] == 1, ('Batch size must be 1.')
+    # parse lattice_kwargs
+    if lattice_kwargs is not None:
+        lattice_kwargs.setdefault('rotate', 0.)
+        rotate = lattice_kwargs.pop('rotate')
+        if type(rotate) is not type(lambda : 0.):
+            rotate_fn = lambda : rotate
+        else:
+            rotate_fn = rotate
+    else:
+        rotate_fn = None
+    # counter
+    cnt = 0.
+    n_kernels = model.layers[layer_id].forward_layer.pool.mu.shape[0]
+    RF_acc = torch.zeros(n_kernels)
+    correct = 0.
+    # get SNR, accuracy for each image
+    for i, ((target, label), (crowd, _)) in enumerate(zip(target_loader, crowd_loader)):
+        # reset RFs
+        if lattice_fn is not None and lattice_kwargs is not None:
+            if rotate_fn is not None:
+                mu, sigma = lattice_fn(**lattice_kwargs, rotate=rotate_fn())
+            else:
+                mu, sigma = lattice_fn(**lattice_kwargs) 
+            model.layers[layer_id].forward_layer.pool.update_rfs(mu=mu, sigma=sigma)
+        # get mask
+        if RF_mask is not None:
+            mask_i = RF_mask.clone()
+        else:
+            mask_i = model.rf_index(target, layer_id, thr=0.1).float()
+        # attention
+        if extent:
+            model = apply_attention_field(model, layer_id, mu, sigma, [26,26], extent)
+        # get output for each RF
+        with torch.no_grad():
+            crowd_output = model.rf_output(target + crowd, layer_id, retain_shape=True)
+            # mask crowd_output and pass forward to get accuracy
+            masked_output = torch.mul(crowd_output, mask_i.reshape(1, 1, -1, 1, 1))
+            # permute dimensions to (n_RF, n_ch, h, w)
+            masked_output = masked_output[0].permute(1,0,2,3)
+            # get flattened output of third layer (n_RF, n_ch, h * w)
+            output = torch.flatten(model.apply_layers(masked_output, ['2']), -2)
+            output = torch.mul(output, mask_i.reshape(-1, 1, 1))
+            # get prediction and accuracy
+            pred = torch.max(torch.max(torch.sum(output, 0), -1)[0], 0)[1]
+            correct += (pred == label).item()
+            cnt += 1.
+            # for each RF, get predicted target without RF
+            for idx in torch.where(mask_i)[1].tolist():
+                tmp_mask = mask_i.clone()
+                tmp_mask[0,idx] = 0.
+                tmp_output = torch.mul(output, tmp_mask.reshape(-1, 1, 1))
+                pred = torch.max(torch.max(torch.sum(tmp_output, 0), -1)[0], 0)[1]
+                RF_acc[idx] += (pred == label).item()
+    RF_acc = (correct - RF_acc) / cnt
+    correct /= cnt
+    return RF_acc, correct
